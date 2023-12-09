d1d5e0fed375f6a5593c2d3d5369e7dae1cfe320
terezaRafajovaLEGO <tereza.rafajova@lego.com>
2023-12-09 12:14:22 +0100

    testing

--- Bachelor_API.Tests/UnitTest1.cs
+++ Bachelor_API.Tests/UnitTest1.cs
@@ -1,5 +1,9 @@
+using System.Drawing.Printing;
 using Bachelor_API.Data;
 using Bachelor_API.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.InMemory;
 
@@ -8,28 +12,171 @@ namespace Bachelor_API.Tests
     public class LessonEndpointsTests
     {  
         [Fact]
-        public async Task GetAllLessons_ReturnsAllLessons()
+        public async Task ShareLesson_TestResult()
         {
-            // Arrange
             var dbContextOptions = new DbContextOptionsBuilder<Bachelor_APIContext>()
                 .UseInMemoryDatabase(databaseName: "TestDatabase")
                 .Options;
 
             using (var dbContext = new Bachelor_APIContext(dbContextOptions))
             {
-                // Add test data to the in-memory database
-                dbContext.Lesson.Add(new Lesson { LessonId = 1, Title = "Lesson 1" });
-                dbContext.Lesson.Add(new Lesson { LessonId = 2, Title = "Lesson 2" });
-                await dbContext.SaveChangesAsync();
-            }
+                //create lesson to share
+                var lesson = new Lesson
+                {
+                    Title = "TestLeson",
+                    Username = "TestUser",
+                    SharingTime = 120,
+                    NumberOfPages = 1,
+                    Descriptions = new List<Description>
+                    {
+                        new Description
+                        {
+                            Slot = 2,
+                            Text = "TestTextPage1",
+                            PageNumber = 1
+                        },
+                        new Description
+                        {
+                            Slot = 4,
+                            Text = "TestTextPage2",
+                            PageNumber = 1
+                        }
+                    },
+                    CodeBlocks = new List<CodeBlock>
+                    {
+                        new CodeBlock
+                        {
+                            Slot = 3,
+                            JsonBlocks = "Blocks Json",
+                            PageNumber = 1
+                        },
+                        new CodeBlock
+                        {
+                            Slot = 5,
+                            JsonBlocks = "Blocks Json",
+                            PageNumber = 1
+                        }
+                    },
+                    Titles = new List<Title>
+                    {
+                        new Title
+                        {
+                            Slot = 1,
+                            Text = "TestTitlePage1",
+                            PageNumber = 1
+                        }
+                    }
+                };
 
-            using (var dbContext = new Bachelor_APIContext(dbContextOptions))
+                var result = await LessonEndpoints.ShareLesson(lesson, dbContext);
+                // Assert 
+                //lesson type
+                var createdLesson = Assert.IsType<Lesson>(result.Value);
+                Assert.NotNull(createdLesson);
+
+                //Lesson sharing code
+                Assert.True(result.Value.SharingCode >= 100000 && result.Value.SharingCode <= 999999);     
+
+                //lesson components
+                Assert.Equal(2, result.Value.Descriptions.Count);       
+                Assert.Equal(2, result.Value.CodeBlocks.Count);       
+                Assert.Single(result.Value.Titles);
+            };
+            
+        }
+
+        [Fact]
+        public async Task GetLessonByCode_ExistingCode()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<Bachelor_APIContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            using var context = new Bachelor_APIContext(options);
+
+            var lesson = new Lesson
             {
-                // Act
-                var result = await LessonEndpoints.GetAllLessons(dbContext);
+                LessonId = 2,
+                Title = "TestLeson",
+                Username = "TestUser",
+                SharingTime = 120,
+                SharingCode = 123456,
+                NumberOfPages = 1,
+                Descriptions = new List<Description>
+                {
+                    new Description
+                    {
+                        Slot = 2,
+                        Text = "TestTextPage1",
+                        PageNumber = 1
+                    },
+                    new Description
+                    {
+                        Slot = 4,
+                        Text = "TestTextPage2",
+                        PageNumber = 1
+                    }
+                },
+                CodeBlocks = new List<CodeBlock>
+                {
+                    new CodeBlock
+                    {
+                        Slot = 3,
+                        JsonBlocks = "Blocks Json",
+                        PageNumber = 1
+                    },
+                    new CodeBlock
+                    {
+                        Slot = 5,
+                        JsonBlocks = "Blocks Json",
+                        PageNumber = 1
+                    }
+                },
+                Titles = new List<Title>
+                {
+                    new Title
+                    {
+                        Slot = 1,
+                        Text = "TestTitlePage1",
+                        PageNumber = 1
+                    }
+                }
+            };
+
+            context.Lesson.Add(lesson);
+            await context.SaveChangesAsync();
+
+            var result = await LessonEndpoints.GetLessonByCode(123456, context);
+
+            // Assert
+            var actionResult = Assert.IsType<Results<Ok<Lesson>, NotFound>>(result);
+            var lessonResult = Assert.IsType<Ok<Lesson>>(actionResult.Result);
+            var lessonValue = lessonResult.Value;
+            Assert.Equal(lesson.LessonId, lessonValue.LessonId);
+            Assert.Equal(lesson.SharingCode, lessonValue.SharingCode);
+            Assert.Equal(lesson.Title, lessonValue.Title);
+            Assert.Equal(lesson.Username, lessonValue.Username);
+            Assert.Equal(lesson.SharingTime, lessonValue.SharingTime);
+            Assert.Equal(lesson.NumberOfPages, lessonValue.NumberOfPages);
+            Assert.Equal(lesson.Descriptions.Count, lessonValue.Descriptions.Count);
+            Assert.Equal(lesson.CodeBlocks.Count, lessonValue.CodeBlocks.Count);   
+            Assert.Equal(lesson.Titles.Count, lessonValue.Titles.Count);
+        }
+        [Fact]
+        public async Task GetLessonByCode_NonExistingCode()
+        {
+            var options = new DbContextOptionsBuilder<Bachelor_APIContext>()
+                .UseInMemoryDatabase(databaseName: "TestDb")
+                .Options;
+
+            using (var context = new Bachelor_APIContext(options))
+            {
+                var result = await LessonEndpoints.GetLessonByCode(123457, context);	
 
                 // Assert
-                Assert.Equal(2, result.Length);
+                var actionResult = Assert.IsType<Results<Ok<Lesson>, NotFound>>(result);
+                Assert.IsType<NotFound>(actionResult.Result);
             }
         }
     
--- Bachelor_API/LessonEndpoints.cs
+++ Bachelor_API/LessonEndpoints.cs
@@ -4,6 +4,7 @@ using Bachelor_API.Model;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.OpenApi;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 namespace Bachelor_API;
 
 public static class LessonEndpoints
@@ -16,6 +17,16 @@ public static class LessonEndpoints
         .WithName("GetAllLessons")
         .WithOpenApi();
 
+        group.MapGet("/code/{code}", GetLessonByCode)
+        .WithName("GetLessonByCode")
+        .WithOpenApi();
+
+        group.MapPost("/", ShareLesson)
+        .WithName("CreateLesson")
+        .WithOpenApi()
+        .RequireAuthorization();
+
+
         group.MapGet("/id/{id}", async Task<Results<Ok<Lesson>, NotFound>> (int id, Bachelor_APIContext db) =>
         {
             return await db.Lesson.AsNoTracking()
@@ -25,22 +36,6 @@ public static class LessonEndpoints
                     : TypedResults.NotFound();
         })
         .WithName("GetLessonById")
-        .WithOpenApi();
-
-        group.MapGet("/code/{code}", async Task<Results<Ok<Lesson>, NotFound>> (int code, Bachelor_APIContext db) =>
-        {
-            var lesson = await db.Lesson
-            .Include(l => l.CodeBlocks)
-            .Include(l => l.Descriptions)
-            .Include(l => l.Titles)
-            .AsNoTracking()
-            .FirstOrDefaultAsync(model => model.SharingCode == code);
-
-            return lesson != null
-                ? TypedResults.Ok(lesson)
-                : TypedResults.NotFound();
-        })
-        .WithName("GetLessonByCode")
         .WithOpenApi();
 
         group.MapPut("/{id}", async Task<Results<Ok, NotFound>> (int lessonid, Lesson lesson, Bachelor_APIContext db) =>
@@ -59,22 +54,7 @@ public static class LessonEndpoints
         })
         .WithName("UpdateLesson")
         .WithOpenApi();
-
-        group.MapPost("/", async (Lesson lesson, Bachelor_APIContext db) =>
-        {
-            var codeGenerator = new CodeGenerator(db);
-
-            int uniqueCode = codeGenerator.GenerateUniqueCode();
-            lesson.SharingCode = uniqueCode;
-
-            db.Lesson.Add(lesson);
-            await db.SaveChangesAsync();
-            return TypedResults.Created($"/api/Lesson/{lesson.LessonId}",lesson);
-        })
-        .WithName("CreateLesson")
-        .WithOpenApi()
-        .RequireAuthorization();
-
+        
         group.MapDelete("/{id}", async Task<Results<Ok, NotFound>> (int lessonid, Bachelor_APIContext db) =>
         {
             var affected = await db.Lesson
@@ -91,4 +71,30 @@ public static class LessonEndpoints
         return await db.Lesson.ToArrayAsync();
 
     }
+
+    public static async Task<Results<Ok<Lesson>, NotFound>> GetLessonByCode (int code, Bachelor_APIContext db)
+        {
+            var lesson = await db.Lesson
+            .Include(l => l.CodeBlocks)
+            .Include(l => l.Descriptions)
+            .Include(l => l.Titles)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(model => model.SharingCode == code);
+
+            return lesson != null
+                ? TypedResults.Ok(lesson)
+                : TypedResults.NotFound();
+        }
+
+    public static async Task<Created<Lesson>> ShareLesson(Lesson lesson, Bachelor_APIContext db)
+        {
+            var codeGenerator = new CodeGenerator(db);
+
+            int uniqueCode = codeGenerator.GenerateUniqueCode();
+            lesson.SharingCode = uniqueCode;
+
+            db.Lesson.Add(lesson);
+            await db.SaveChangesAsync();
+            return TypedResults.Created($"/api/Lesson/{lesson.LessonId}",lesson);
+        }
 }
